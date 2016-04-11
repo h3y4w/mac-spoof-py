476680398e735f56c14616d9d3e017f38d5db53b
Heyaw Meteke <heyawmeteke@gmail.com>
2016-04-11 00:40:45 -0700

    added menu

--- mac-spoof.py
+++ mac-spoof.py
@@ -2,8 +2,9 @@
 import subprocess
 import os
 from sys import argv
-
-def main():
+		
+def initial_setup():
+	
 	p = subprocess.Popen('ip route ls', stdout=subprocess.PIPE, shell=True)
 	(tempDevice, err) = p.communicate()
 	device_index=tempDevice.find('dev')+3
@@ -17,10 +18,23 @@ def main():
 	p = subprocess.Popen("whoami", stdout=subprocess.PIPE, shell=True)
 	(user, err) = p.communicate()
 	user = user.replace("\n", "")
-	macDir = '/sys/class/net/wlan0/subsystem/wlan0/address'
+	macDir = '/sys/class/net/%s/subsystem/%s/address' % (device,device)
 	os.chdir('/home/deno/')
 	with open(macDir,'r') as defaultMacFile:
 		defaultMac=defaultMacFile.read().replace('\n', '')
+		mac=defaultMac
+	menu(mac,device)
+	
+def menu(mac,device):
+	print '\t\t\t\tMac Spoofer\n\t\t\t\t~~~~~~~~~~~'
+	print 'Current Device: %s' %(device)
+	print 'Current MAC: %s\n\n' % (mac)
+	print '\t1)Random\n'
+	print '\t\t2)Input\n'
+	print '\t\t\t3)Reset\n'
+	print '\t\t\t\t4)Change Device\n'
+	print '\t\t\t\t\t\t5)Exit\n\n'
+	print '>> '
 
-
-main()
+	
+initial_setup()
